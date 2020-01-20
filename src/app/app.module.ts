528a70801d8b0ddd0f56d6057871bc2acdcd1f6d
Kevin Stamps <kstamps2484@gmail.com>
2020-01-20 14:30:11 -0700

    add bulma to project with global-styles sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule
